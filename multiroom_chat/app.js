495705447a75bf1e4faa284bd575b330feaad28d
Gabriel Dezan Busarello <gabrielbusarello@univille.br>
2018-04-19 11:39:01 -0300

    Enviando mensagem quando usuário se conecta no chat

--- multiroom_chat/app.js
+++ multiroom_chat/app.js
@@ -1,5 +1,18 @@
 var app =  require("./config/server");
 
-app.listen(80, function() {
+var server = app.listen(80, function() {
     console.log('Servidor Online');
 });
+
+var io = require('socket.io').listen(server);
+
+app.set('io', io);
+
+io.on('connection', function(socket) {
+    console.log('Usuário conectou');
+
+    socket.on('disconnect', function() {
+        console.log('Usuário desconectou');
+    });
+});
+
