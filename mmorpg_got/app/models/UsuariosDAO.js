1bcd730feb5dd3e6c24e173c2afaa75ada3b53ab
Gabriel Dezan Busarello <gabrielbusarello@univille.br>
2018-04-24 09:49:07 -0300

    Feito a criptografia da senha

--- mmorpg_got/app/models/UsuariosDAO.js
+++ mmorpg_got/app/models/UsuariosDAO.js
@@ -1,3 +1,5 @@
+var crypto = require('crypto');
+
 function UsuariosDAO(connection){
 	this._connection = connection();
 }
@@ -5,6 +7,9 @@ function UsuariosDAO(connection){
 UsuariosDAO.prototype.inserirUsuario = function(usuario){
 	this._connection.open( function(err, mongoclient){
 		mongoclient.collection("usuarios", function(err, collection){
+
+			usuario.senha = crypto.createHash("md5").update(usuario.senha).digest("hex");
+
 			collection.insert(usuario);
 
 			mongoclient.close();
@@ -15,6 +20,7 @@ UsuariosDAO.prototype.inserirUsuario = function(usuario){
 UsuariosDAO.prototype.autenticar = function(usuario, req, res){
 	this._connection.open( function(err, mongoclient){
 		mongoclient.collection("usuarios", function(err, collection){
+			usuario.senha = crypto.createHash("md5").update(usuario.senha).digest("hex");
 			collection.find(usuario).toArray(function(err, result) {
 				if(result[0] != undefined)  {
 					req.session.autorizado = true;
